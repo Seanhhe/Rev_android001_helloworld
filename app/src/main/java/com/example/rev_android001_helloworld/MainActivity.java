57c5469dbf4ebe9b22b15f41ad1256f085f25be5
Seanhhe <seanhhe@gmail.com>
2020-07-04 16:56:15 +0800

    2020-07-04

---
+++ app/src/main/java/com/example/rev_android001_helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rev_android001_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
