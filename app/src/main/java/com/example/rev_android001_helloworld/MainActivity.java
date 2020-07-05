3488d589631efc3cb4f9a50bcbc4e7c0214425b5
Seanhhe <seanhhe@gmail.com>
2020-07-05 18:00:59 +0800

    2020-07-05_增加註解

--- app/src/main/java/com/example/rev_android001_helloworld/MainActivity.java
+++ app/src/main/java/com/example/rev_android001_helloworld/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.rev_android001_helloworld;
 
+/*    安卓手機的開發人員選項：
+        開啟步驟 : 進入"設定 > 關於手機 > 軟體資訊 > 輕觸「版本號碼」七次"。
+        關閉步驟 : 開啟"設定 > 應用程式 > 設定 > 儲存位置 > 清除資料 > 刪除"。
+ */
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
